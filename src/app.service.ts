f83ab176ea5c48ce8b01b75efc532d701fb9cea9
59 <5.9.devv@gmail.com>
2023-03-19 00:16:46 +0900

    eslint, pretteir 설정

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common"
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    var foo = {
+      bar: "bar",
+      hel: "hel",
+    }
+    return "Hello World!"
   }
 }
