aaeb7790a31d32ee74adb5d6c5bf51704d554920
59 <5.9.devv@gmail.com>
2023-03-20 18:03:15 +0900

    boards api with typeorm

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,45 +3,60 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   UsePipes,
   ValidationPipe,
 } from "@nestjs/common"
 import { BoardsService } from "./boards.service"
-import { Board, BoardStatus } from "./boards.model"
+import { BoardStatus } from "./board-status.enum"
 import { CreateBoardDto } from "./dto/create-board.dto"
 import { BoardStatusValidationPipe } from "./pipe/board-status-validation.pipe"
+import { Board } from "./board.entity"
 
 @Controller("boards")
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
   @Get()
-  getAllBoards(): Array<Board> {
-    return this.boardService.getAllBoards()
+  getAllBoards(): Promise<Array<Board>> {
+    const result = this.boardService.getAllBoards()
+    return result
   }
 
   @Post()
   @UsePipes(ValidationPipe)
-  crateBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  crateBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardService.createBoard(createBoardDto)
   }
 
-  @Get("/:id")
-  getBoardById(@Param("id") id): Board {
-    return this.boardService.getBoardById(id)
-  }
-
   @Delete("/:id")
-  deleteBoardById(@Param("id") id): void {
-    this.boardService.deleteBoardById(id)
+  deleteBoard(@Param("id", ParseIntPipe) id): Promise<any> {
+    return this.boardService.deleteBoard(id)
   }
 
   @Patch("/:id/status")
-  updateBoardStatus(@Param("id") id: string, @Body("status", BoardStatusValidationPipe) status: BoardStatus): Board {
-    return this.boardService.updateBoardStatusById(id, status)
+  updateBoardStatus(
+    @Param("id", ParseIntPipe) id,
+    @Body("status", BoardStatusValidationPipe) status: BoardStatus
+  ): Promise<Board> {
+    return this.boardService.updateBoardStatus(id, status)
   }
+
+  // @Get("/:id")
+  // getBoardById(@Param("id") id): Board {
+  //   return this.boardService.getBoardById(id)
+  // }
+
+  // @Delete("/:id")
+  // deleteBoardById(@Param("id") id): void {
+  //   this.boardService.deleteBoardById(id)
+  // }
+
+  // @Patch("/:id/status")
+  // updateBoardStatus(@Param("id") id: string, @Body("status", BoardStatusValidationPipe) status: BoardStatus): Board {
+  //   return this.boardService.updateBoardStatusById(id, status)
+  // }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,57 +1,86 @@
 import { Injectable, NotFoundException } from "@nestjs/common"
-import { Board, BoardStatus } from "./boards.model"
 import { v1 as uuid } from "uuid"
 import { CreateBoardDto } from "./dto/create-board.dto"
+import { BoardStatus } from "./board-status.enum"
+import { Repository } from "typeorm"
+import { InjectRepository } from "@nestjs/typeorm"
+import { Board } from "./board.entity"
 
 @Injectable()
 export class BoardsService {
-  private boards: Array<Board> = []
+  constructor(@InjectRepository(Board) private readonly boardRepository: Repository<Board>) {}
 
   // 전체 게시글 가져오기
-  getAllBoards(): Array<Board> {
-    return this.boards
+  async getAllBoards(): Promise<Array<Board>> {
+    const boards = await this.boardRepository.find()
+    return boards
   }
 
   // 게시글 작성하기
-  createBoard(createBoardDto: CreateBoardDto) {
-    const board: Board = {
-      id: uuid(),
-      title: createBoardDto.title,
-      description: createBoardDto.description,
-      status: BoardStatus.PUBLIC,
-    }
-
-    this.boards.push(board)
-    return board
-  }
-
-  // ID로 게시글 가져오기
-  getBoardById(id: string): Board {
-    const board: Board = this.boards.find((board) => board.id === id)
-
-    if (!board) {
-      throw new NotFoundException(`id not exists(${id})`)
-    }
-
-    return board
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    const board = this.boardRepository.create(createBoardDto)
+    return await this.boardRepository.save(board)
   }
 
   // 게시글 삭제하기
-  deleteBoardById(id: string) {
-    const foundBoard: Board = this.getBoardById(id)
+  async deleteBoard(id: number): Promise<any> {
+    const result = await this.boardRepository.delete(id)
 
-    if (!foundBoard) {
-      throw new NotFoundException("Board Not Found")
+    if (result.affected === 0) {
+      throw new NotFoundException(`Can't find board with id ${id}`)
+    }
+    return result
+  }
+
+  // 게시글 수정하기
+  async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+    const board = await this.boardRepository.findOneBy({ id: id })
+    console.log(board)
+
+    if (!board) {
+      throw new NotFoundException(`Can't find board with id ${id}`)
     }
 
-    this.boards.filter((board) => board.id !== foundBoard.id)
-  }
-
-  // 게시글 상태 수정하기
-  updateBoardStatusById(id: string, status: BoardStatus): Board {
-    const board = this.boards.find((board) => board.id === id)
     board.status = status
-
+    await this.boardRepository.save(board)
     return board
   }
+  // constructor(private readonly boardRepository: BoardRepository) {}
+  // // 전체 게시글 가져오기
+  // getAllBoards(): Array<Board> {
+  //   return this.boards
+  // }
+  // // 게시글 작성하기
+  // createBoard(createBoardDto: CreateBoardDto) {
+  //   const board: Board = {
+  //     id: uuid(),
+  //     title: createBoardDto.title,
+  //     description: createBoardDto.description,
+  //     status: BoardStatus.PUBLIC,
+  //   }
+  //   this.boards.push(board)
+  //   return board
+  // }
+  // // ID로 게시글 가져오기
+  // getBoardById(id: string): Board {
+  //   const board: Board = this.boards.find((board) => board.id === id)
+  //   if (!board) {
+  //     throw new NotFoundException(`id not exists(${id})`)
+  //   }
+  //   return board
+  // }
+  // // 게시글 삭제하기
+  // deleteBoardById(id: string) {
+  //   const foundBoard: Board = this.getBoardById(id)
+  //   if (!foundBoard) {
+  //     throw new NotFoundException("Board Not Found")
+  //   }
+  //   this.boards.filter((board) => board.id !== foundBoard.id)
+  // }
+  // // 게시글 상태 수정하기
+  // updateBoardStatusById(id: string, status: BoardStatus): Board {
+  //   const board = this.boards.find((board) => board.id === id)
+  //   board.status = status
+  //   return board
+  // }
 }
